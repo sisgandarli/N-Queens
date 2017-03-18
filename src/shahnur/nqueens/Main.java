40c02517f31bb899a769f1e2008a6d9698530ed2
Shahnur Isgandarli <sisgandarli2018@ada.edu.az>
2017-03-18 17:02:57 +0400

    Javadoc Comments have been added for Main.java file

--- src/shahnur/nqueens/Main.java
+++ src/shahnur/nqueens/Main.java
@@ -2,7 +2,15 @@ package shahnur.nqueens;
 
 import javafx.application.Application;
 
+/**
+@author Shahnur Isgandarli
+ */
+
 public class Main {
+    /**
+     * This is main method of the application.
+     * @param args The arguments.
+     */
     public static void main(String[] args) {
         Application.launch(Runner.class, args);
     }
